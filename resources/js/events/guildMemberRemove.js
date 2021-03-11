98740e818d50868db57351cf9620b744000c4c28
Ryan <rsbb27@gmail.com>
2021-03-11 14:42:16 -0500

    fixed ejection message

--- resources/js/events/guildMemberRemove.js
+++ resources/js/events/guildMemberRemove.js
@@ -4,5 +4,5 @@ module.exports = (client, member) => {
     let logs = member.guild.channels.cache.find(i => i.name === 'logs');
     logs.send(`${member.displayName} (${member.user.username}) left ${member.guild}`);
     
-    member.guild.channels.cache.find(i => i.name === 'crew-chat').send(`${member} got ejected.`);
+    member.guild.channels.cache.find(i => i.name === 'crew-chat').send(`${member.user.username} got ejected.`);
 }
