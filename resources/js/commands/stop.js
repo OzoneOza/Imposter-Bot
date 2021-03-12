2727356631b9cba5424bef665fca54891fdfa006
Ryan <rsbb27@gmail.com>
2021-03-12 13:04:43 -0500

    const to let

--- resources/js/commands/stop.js
+++ resources/js/commands/stop.js
@@ -3,9 +3,9 @@ module.exports = {
     args: '',
     description: 'Stops the song and clears the queue',
     run(client, msg)  {
-        const serverQueue = msg.client.queue.get(msg.guild.id);
+        let serverQueue = msg.client.queue.get(msg.guild.id);
 
-        const voiceChannel = msg.member.voice.channel;
+        let voiceChannel = msg.member.voice.channel;
         if (!voiceChannel) return msg.channel.send(`You need to be in a voice channel before you can stop the music!`);
         
         serverQueue.songs = [];
@@ -15,6 +15,6 @@ module.exports = {
             serverQueue.connection.dispatcher.end();
         }
 
-        msg.channel.send(':wave:  Music has been stopped and the queue has been cleared!  :wave:');
+        msg.channel.send(':octagonal_sign:  Music has been stopped and the queue has been cleared!');
     }
 }
