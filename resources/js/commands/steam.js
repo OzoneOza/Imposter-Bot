ac80fb811c6f6df3e98de8f8eb9242baaf4b3347
Ryan <rsbb27@gmail.com>
2021-03-12 12:53:11 -0500

    added channel ID support

--- resources/js/commands/steam.js
+++ resources/js/commands/steam.js
@@ -17,25 +17,25 @@ module.exports = {
         }
     },
     async readEmail(client, msg) {
-        const gmail = google.gmail({ version: 'v1', auth: client.oAuth2Client });
+        let gmail = google.gmail({ version: 'v1', auth: client.oAuth2Client });
 
-        const listRes = await gmail.users.messages.list({userId: 'me'})
+        let listRes = await gmail.users.messages.list({userId: 'me'})
         .catch(error => {console.log(`The API returned an error: ${error}`);});
     
-        const emails = listRes.data.messages;
+        let emails = listRes.data.messages;
     
         if (emails.length == 0) {
             msg.author.send('No verification code found. Try resending the verification email, or wait a few minutes.');
         } else {
             for (email of emails) {
-                const getRes = await gmail.users.messages.get({userId: 'me', id: email.id})
+                let getRes = await gmail.users.messages.get({userId: 'me', id: email.id})
                 .catch(error => {console.log(`The API returned an error: ${error}`);});
     
-                const snippet = getRes.data.snippet;
+                let snippet = getRes.data.snippet;
     
                 if (snippet.includes('Steam Guard code')) {
-                    const guild = client.guilds.cache.find(i => i.name === 'Skip on Six');
-                    const logs = guild.channels.cache.find(i => i.name === 'logs');
+                    let guild = client.guilds.cache.find(i => i.id === client.config.guildID);
+                    let logs = guild.channels.cache.find(i => i.id === client.config.logsChannelID);
     
                     msg.author.send(`Verification code: ${snippet.slice(91, 96)}`);
                     logs.send(`${msg.author.username} is attempting to access the steam account`);
