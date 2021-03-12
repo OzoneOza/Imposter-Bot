b6b7bfbfc0fbfad34566974ab33662a21fb7683a
Ryan <rsbb27@gmail.com>
2021-03-12 13:09:07 -0500

    added channel ID support

--- resources/js/events/guildMemberRemove.js
+++ resources/js/events/guildMemberRemove.js
@@ -1,8 +1,8 @@
 module.exports = (client, member) => {
     if (member.user.bot) return;
 
-    let logs = member.guild.channels.cache.find(i => i.name === 'logs');
+    let logs = member.guild.channels.cache.find(i => i.id === client.config.logsChannelID);
     logs.send(`${member.displayName} (${member.user.username}) left ${member.guild}`);
     
-    member.guild.channels.cache.find(i => i.name === 'crew-chat').send(`${member.user.username} got ejected.`);
+    member.guild.channels.cache.find(i => i.id === client.config.generalChannelID).send(`${member.user.username} got ejected.`);
 }
